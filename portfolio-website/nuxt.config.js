0ba0c3387966befc4661606ad2261534d659bf0e
Soumya <iamsoumyaagrawal@gmail.com>
2023-08-30 17:54:59 +0530

    trying to deploy

--- portfolio-website/nuxt.config.js
+++ portfolio-website/nuxt.config.js
@@ -3,7 +3,10 @@ import colors from 'vuetify/es5/util/colors'
 export default {
   // Disable server-side rendering: https://go.nuxtjs.dev/ssr-mode
   ssr: false,
-
+  generate: {
+    fallback: true
+  },
+  target: 'static',
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
     titleTemplate: '%s - portfolio-website',
